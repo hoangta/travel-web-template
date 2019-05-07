12080c3f17d114f83e144e6cbcf460ffaa25bab7
Hoang Ta <vinhhoang.ta@2359media.com>
2019-05-08 01:45:27 +0700

    Updates

---
+++ precache-manifest.d80d2fe12a42ac24c20536948f7913d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5f521410969cd44c0f947e2f72b834",
+    "url": "/travel-web-template/index.html"
+  },
+  {
+    "revision": "7e046df6147efcd2eed1",
+    "url": "/travel-web-template/static/css/main.605893ae.chunk.css"
+  },
+  {
+    "revision": "4a522889b1c73b9e42c7",
+    "url": "/travel-web-template/static/js/2.ef54362d.chunk.js"
+  },
+  {
+    "revision": "7e046df6147efcd2eed1",
+    "url": "/travel-web-template/static/js/main.4e92e2a5.chunk.js"
+  },
+  {
+    "revision": "1082bc506750d6302f39",
+    "url": "/travel-web-template/static/js/runtime~main.b5d66598.js"
+  }
+]);
